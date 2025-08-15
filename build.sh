3f62f6b533f8778f2bbf8555a8a16af65fd81bd0
Harold Brenes <harold@hbrenes.com>
2025-08-15 16:44:20 -0400

    Update build script

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-cmake -C cmake/caches/PredefinedParams.cmake -DCMAKE_BUILD_TYPE=Release -B build -DLLVM_PARALLEL_COMPILE_JOBS=10 -DLLVM_PARALLEL_LINK_JOBS=10 -DHLSL_INCLUDE_TESTS=OFF -DSPIRV_BUILD_TESTS=OFF
+cmake -B build -C cmake/caches/PredefinedParams.cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_PARALLEL_COMPILE_JOBS=10 -DLLVM_PARALLEL_LINK_JOBS=10 -DHLSL_INCLUDE_TESTS=OFF -DSPIRV_BUILD_TESTS=OFF
 
 cmake --build build --config Release --target dxcompiler -j10
 cmake --build build --config Release --target dxc -j10
 
 install_name_tool -delete_rpath @executable_path/../lib build/bin/dxc
-install_name_tool -add_rpath @executable_path build/bin/dxc
+install_name_tool -add_rpath    @executable_path build/bin/dxc
+
+cmake --install build --component dxc --prefix .dist
+cmake --install build --component dxcompiler --prefix .dist
